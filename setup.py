263409257dd019a0ca5f45110710ec82484d23e5
WataruTakahagi <watarut@outlook.com>
2019-07-02 13:37:15 +0900

    Create setup-tool

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='Analyst',
+    version='0.1.0',
+    author='Wataru Takahagi',
+    author_email='watarut@eqchem.s.u-tokyo.ac.jp',
+    url='https://github.com/utokyo-hirata-lab/Analyst',
+    py_modules=['hrtlab_core'],
+    description='Analysis tools for Nu Plasma 2, iCap TQ and Attom',
+    keywords='visualization data-science',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
